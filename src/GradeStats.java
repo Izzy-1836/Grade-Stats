9ab66a2c4e977d9354917f517d430a5df78633d0
izzie <izzie@LAPTOP-DRVD4B4P.lan>
2025-10-20 02:02:24 -0500

    2nd initial push

--- src/GradeStats.java
+++ src/GradeStats.java
@@ -3,7 +3,16 @@ import java.util.Scanner;
 public class GradeStats {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		double[] grades = new double[10];
+		double sum = 0;
+		double max = Double.MIN_VALUE;
+		double min = Double.MAX_VALUE;
+		
+		for (int i = 0; i < grades.length; i++) {
+			System.out.print("Enter grade #" + (i + 1) + ": ");
+			
+		}
 
 	}
 
