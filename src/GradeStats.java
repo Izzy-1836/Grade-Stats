49f915aaf421ebd2a3d57aeb672b9aa9d70b1611
izzie <izzie@LAPTOP-DRVD4B4P.lan>
2025-10-20 02:10:55 -0500

    second push

--- src/GradeStats.java
+++ src/GradeStats.java
@@ -11,7 +11,23 @@ public class GradeStats {
 		
 		for (int i = 0; i < grades.length; i++) {
 			System.out.print("Enter grade #" + (i + 1) + ": ");
+			while (!input.hasNextDouble()) {
+				System.out.println("Invaild input. Please enter a number.");
+				input.next();
+			}
+			grades[i] = input.nextDouble();
+			sum+= grades[i];
 			
+			if (grades[i] > max) {
+				max = grades[i];
+			}
+			if (grades[i] < min) {
+				min = grades[i];
+			}
+		}
+		
+		
+			}
 		}
 
 	}
